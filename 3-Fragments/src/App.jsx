55f2919ba3c151e57fc36bb1fa24a7dfb75ff6d8
sagarchaudhary75jaat <sagarchaudhary75jaat@gmail.com>
2026-06-29 10:17:12 +0530

    New Change

--- 3-Fragments/src/App.jsx
+++ 3-Fragments/src/App.jsx
@@ -2,6 +2,7 @@ import FoodItems from "./components/FoodItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   let foodItems = [
@@ -15,10 +16,16 @@ function App() {
 
   return (
     <>
-      <h1  className="food-heading">Healthy Food</h1>
+      <Container>
+        <h1  className="food-heading">Healthy Food</h1>
       <ErrorMessage item={foodItems} />
       <FoodItems item={foodItems} />
-    </>
+      </Container>
+      <Container>
+        <p> Above is the list of Healthy foods that are good for your health and well being.</p>
+      </Container>
+      </>
+    
   );
 }
 
--- 3-Fragments/src/components/Item.jsx
+++ 3-Fragments/src/components/Item.jsx
@@ -1,6 +1,11 @@
-import styles from"./Item.module.css"
+import styles from "./Item.module.css";
+
 const Item = ({ food }) => {
-  return <li className={`${styles['Kg-item']} `}><span className={styles['Kg-span']}>{food}</span></li>;
+  return (
+    <li className={`list-group-item ${styles["Kg-item"]}`}>
+      <span className={styles["Kg-span"]}>{food}</span>
+    </li>
+  );
 };
 
 export default Item;
