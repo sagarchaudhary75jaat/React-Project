6f85b38e6512f4d2f473e614f224e406332c6caa
sagarchaudhary75jaat <sagarchaudhary75jaat@gmail.com>
2026-06-29 08:38:01 +0530

    Update Todo-app-version-1

--- Todo-app-version-1/src/components/ToDoItems.jsx
+++ Todo-app-version-1/src/components/ToDoItems.jsx
@@ -1,6 +1,7 @@
+import styles from"./ToDoItems.module.css"
 import ToDoItem from "./ToDoItem"
 const ToDoItems=({todoItems})=>{
-    return( <div className="itemContainer">
+    return( <div className={styles.itemsContainer}>
         {todoItems.map(item=> <ToDoItem tododate={item.duedate} todoname={item.name}></ToDoItem>)}
         
         
